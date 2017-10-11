64b7041b718c24c97a0e1b1ba7991a465b9c0ca0
harishreddymaryada <maryadareddys@gmail.com>
2017-10-11 10:03:44 +0530

    First Commit

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="Single-photo">
+        i'm the single
+      </div>
+    )
+  }
+});
+
+export default Single;
